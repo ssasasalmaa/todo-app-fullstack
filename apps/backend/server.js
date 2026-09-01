61bf66aa32ba3698a834f20c769f778dfe8512ef
salsabila1901 <salsabblz45@gmail.com>
2026-09-01 13:20:55 +0700

    update

--- apps/backend/server.js
+++ apps/backend/server.js
@@ -19,7 +19,7 @@ app.use(cors());
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/todos', todoRoutes);
-app.use(errorHandler);
+//app.use(errorHandler);
 
 app.get('/', (req, res) => {
   res.send('API Todo App is running smoothly! 🚀');
